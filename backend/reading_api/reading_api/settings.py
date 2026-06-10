0406c4438b82c8a8525c464cca7971057b073cd2
Ivanovswork <destira@mail.ru>
2026-06-10 14:32:01 +0300

    fix: add STATIC_ROOT for collectstatic in production

--- backend/reading_api/reading_api/settings.py
+++ backend/reading_api/reading_api/settings.py
@@ -106,6 +106,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Для collectstatic в production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
